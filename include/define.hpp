1fe1e19c12c9f49f8b8a5a67a34810907e0cd161
yagennankoz <47895949+yagennankoz@users.noreply.github.com>
2024-06-17 22:20:04 +0900

    new

---
+++ include/define.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <Arduino.h>
+
+#define PIN_S1      D27
+#define PIN_S2      D26
+
+#define PIN_BTN1    D19
+#define PIN_BTN2    D18
+
+#define DIR_N       (0u)
+#define DIR_RIGHT   (1u)
+#define DIR_LEFT    (2u)
+
+#define MOUSE_STEP  (6u)
+
+#define INTERVAL      (200000u)
+#define INTERVAL_BTN  (10000u)
+
+#define MY_TUD_HID_REPORT_DESC_MOUSE(...) \
+    HID_USAGE_PAGE ( HID_USAGE_PAGE_DESKTOP     )                 ,\
+    HID_USAGE      ( HID_USAGE_DESKTOP_MOUSE    )                 ,\
+    HID_COLLECTION ( HID_COLLECTION_APPLICATION )                 ,\
+        /* Report ID if any */\
+        __VA_ARGS__ \
+        HID_USAGE          ( HID_USAGE_DESKTOP_POINTER              ) ,\
+        HID_COLLECTION     ( HID_COLLECTION_PHYSICAL                ) ,\
+        /* Button Map */ \
+        HID_USAGE_PAGE     ( HID_USAGE_PAGE_BUTTON                  ) ,\
+        HID_USAGE_MIN      ( 1                                      ) ,\
+        HID_USAGE_MAX      ( 2                                      ) ,\
+        HID_LOGICAL_MIN    ( 0x00                                   ) ,\
+        HID_LOGICAL_MAX    ( 0x01                                   ) ,\
+        HID_REPORT_COUNT   ( 2                                      ) ,\
+        HID_REPORT_SIZE    ( 1                                      ) ,\
+        HID_INPUT          ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
+        HID_REPORT_COUNT   ( 1                                      ) ,\
+        HID_REPORT_SIZE    ( 6                                      ) ,\
+        HID_INPUT          ( HID_CONSTANT | HID_ARRAY | HID_ABSOLUTE ) ,\
+        /* X, Y */ \
+        HID_USAGE_PAGE     ( HID_USAGE_PAGE_DESKTOP                 ) ,\
+        HID_USAGE          ( HID_USAGE_DESKTOP_X                    ) ,\
+        HID_USAGE          ( HID_USAGE_DESKTOP_Y                    ) ,\
+        HID_LOGICAL_MIN    ( 0x81                                   ) ,\
+        HID_LOGICAL_MAX    ( 0x7f                                   ) ,\
+        HID_REPORT_SIZE    ( 8                                      ) ,\
+        HID_REPORT_COUNT   ( 2                                      ) ,\
+        HID_INPUT          ( HID_DATA | HID_VARIABLE | HID_RELATIVE ) ,\
+    HID_COLLECTION_END ,\
+HID_COLLECTION_END
+
+typedef struct MY_TU_ATTR_PACKED
+{
+  uint8_t   buttons;      ///< Buttons mask for currently pressed buttons
+  int8_t    x;            ///< Delta x  movement of left analog-stick
+  int8_t    y;            ///< Delta y  movement of left analog-stick
+}my_hid_mouse_report_t;
---
+++ src/main.cpp
@@ -0,0 +1,128 @@
+#include <Arduino.h>
+#include <stdio.h>
+#include <string.h>
+#include <hardware/gpio.h>
+#include <SPI.h>
+#include <Adafruit_TinyUSB.h>
+#include <define.hpp>
+
+uint8_t const desc_hid_report[] =
+{
+    MY_TUD_HID_REPORT_DESC_MOUSE()
+};
+Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_NONE, 2, false);
+my_hid_mouse_report_t   mouse_report;
+
+uint8_t lastLastSts;
+uint8_t lastSts;
+uint8_t sts;
+uint8_t xorSts;
+uint8_t dir;
+bool inertial = false;
+
+uint8_t lastBtn1Sts;
+uint8_t btn1Sts;
+uint8_t lastBtn2Sts;
+uint8_t btn2Sts;
+
+unsigned long lastTime = micros();
+unsigned long lastTimeBtn1 = micros();
+unsigned long lastTimeBtn2 = micros();
+unsigned long now;
+
+int8_t  cnt;
+
+void paddleIr() {
+  sts = digitalRead(PIN_S1) << 1 | digitalRead(PIN_S2);
+
+  now = micros();
+  if (now < lastTime) lastTime = 0;
+
+  if (inertial && lastTime + INTERVAL < now) {
+    dir = DIR_N;
+  }
+
+  xorSts = lastSts xor sts;
+  if (xorSts == 3) {
+    if (lastLastSts == lastSts) {
+      dir = DIR_N;
+    }
+    if (dir == DIR_RIGHT) {
+      cnt += MOUSE_STEP;
+      inertial = true;
+    } else if (dir == DIR_LEFT) {
+      cnt -= MOUSE_STEP;
+      inertial = true;
+    }
+  } else if (xorSts == 1) {
+    dir = DIR_RIGHT;
+    inertial = false;
+    cnt += MOUSE_STEP;
+  } else {
+    dir = DIR_LEFT;
+    inertial = false;
+    cnt -= MOUSE_STEP;
+  }
+
+  lastLastSts = lastSts;
+  lastSts = sts;
+  lastTime = now;
+}
+
+void setup() {
+  cnt = 0;
+
+  pinMode(PIN_S1, INPUT_PULLUP);
+  pinMode(PIN_S2, INPUT_PULLUP);
+  pinMode(PIN_BTN1, INPUT_PULLUP);
+  pinMode(PIN_BTN2, INPUT_PULLUP);
+
+  TinyUSB_Device_Init(0);
+  usb_hid.begin();
+
+  attachInterrupt(PIN_S1, paddleIr, FALLING);
+  attachInterrupt(PIN_S2, paddleIr, FALLING);
+}
+
+void loop() {
+  mouse_report.y = 0;
+  mouse_report.x = cnt;
+  btn1Sts = !digitalRead(PIN_BTN1);
+  now = micros();
+  if (lastTime > now) {
+    lastTime = 0;
+  }
+  if (lastTimeBtn1 > now) {
+    lastTimeBtn1 = 0;
+  }
+  if (lastTimeBtn2 > now) {
+    lastTimeBtn2 = 0;
+  }
+  if (lastBtn1Sts != btn1Sts) {
+    if (lastTimeBtn1 + INTERVAL_BTN > now) {
+      btn1Sts = lastBtn1Sts;
+    } else {
+      lastTimeBtn1 = now;
+    }
+  }
+
+  btn2Sts = !digitalRead(PIN_BTN2);
+  if (lastBtn2Sts != btn2Sts) {
+    if (lastTimeBtn2 + INTERVAL_BTN > now) {
+      btn2Sts = lastBtn2Sts;
+    } else {
+      lastTimeBtn2 = now;
+    }
+  }
+
+  mouse_report.buttons = btn1Sts | btn2Sts <<1;
+
+  if (TinyUSBDevice.mounted() && usb_hid.ready()) {
+    usb_hid.sendReport(0, &mouse_report, sizeof(mouse_report));
+    cnt = 0;
+  }
+
+  lastBtn1Sts = btn1Sts;
+  lastBtn2Sts = btn2Sts;
+}
+
